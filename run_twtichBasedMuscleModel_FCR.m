b35d539c7068bfc0022efcde21c6e8e2c3261f47
Akira <nagamori@usc.edu>
2018-04-05 18:14:42 -0700

    w/ Tendon

--- run_twtichBasedMuscleModel_FCR.m
+++ run_twtichBasedMuscleModel_FCR.m
@@ -9,11 +9,12 @@ amp_vec = 0.1:0.1:1;
 for i = 1:length(amp_vec)
     
     Data = cell(1,10);
+    tic
     parfor j = 1:10
         output = twitchBasedMuscleModel_FCR(amp_vec(i));
         Data{j} = output;
     end
-    
+    toc
     cd (dataFolder)
     save(['Data' num2str(i)],'Data')
     cd (codeFolder)
