78c32249e93eefb527a8773f5b9f91889c421fe3
Akira <nagamori@usc.edu>
2018-04-11 05:07:31 -0700

    update

--- run_twtichBasedMuscleModel_FCR.m
+++ run_twtichBasedMuscleModel_FCR.m
@@ -6,19 +6,21 @@ dataFolder = '/Volumes/DATA2/Motor Unit Model Data/Twitch-based Muscle Model';
 codeFolder = '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model';
 amp_vec = 0.1:0.1:1;
 
-for i = 6:length(amp_vec)
+for i = 1:length(amp_vec)
+ 
     
-    Data = cell(1,10);
     tic
     %parpool(2)
     for j = 1:10
         output = twitchBasedMuscleModel_FCR(amp_vec(i));
-        Data{j} = output;
+        
+        cd (dataFolder)
+        save(['output_0_1_1_10_l2_' num2str(i) '_' num2str(j)],'output','-v7.3')
+        cd (codeFolder)
+        
+        clear output
     end
-    toc
-    cd (dataFolder)
-    save(['Data_' num2str(i)],'Data','-v7.3')
-    cd (codeFolder)
+    toc   
     
     i
     
