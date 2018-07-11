21faed9d87ebf41c0b3df4a79d905fa0d3fdf0c1
Akira <nagamori@usc.edu>
2018-07-11 09:40:22 -0700

    Update

--- Analysis_Force.m
+++ Analysis_Force.m
@@ -6,7 +6,7 @@ dataFolder = '/Volumes/DATA2/Motor Unit Model Data/Twitch-based Muscle Model';
 codeFolder = '/Users/akira/Documents/GitHub/Twitch-Based-Muscle-Model';
 amp_vec = 0.1:0.1:1;
 
-Fs = 40000;
+Fs = 50000;
 
 mean_Force_all = zeros(1,length(amp_vec));
 SD_Force_all = zeros(1,length(amp_vec));
@@ -16,7 +16,7 @@ CoV_Force_SD_all = zeros(1,length(amp_vec));
 
 for i = 1:length(amp_vec)
     cd (dataFolder)
-    load(['Force_FDI_0_1_1_10_' num2str(i)],'Force')
+    load(['Force_FCR_long_0_1_1_10_' num2str(i)],'Force')
     cd (codeFolder)
     
     mean_Force = zeros(1,10);
@@ -28,6 +28,7 @@ for i = 1:length(amp_vec)
         mean_Force(j) = mean(Force_temp);
         SD_Force(j) = std(Force_temp);
         CoV_Force(j) = SD_Force(j)/mean_Force(j);
+        [pxx(j,:),f] = pwelch(Force(j,:)-mean_Force(j),[],[],0:0.1:50,Fs);
     end
     
     mean_Force_all(i) = mean(mean_Force);
@@ -36,6 +37,11 @@ for i = 1:length(amp_vec)
     CoV_Force_all(i) = mean(CoV_Force);
     CoV_Force_SD_all(i) = std(CoV_Force);
     
+    meanForceAll(:,i) = mean_Force';
+    stdAll(:,i) =  SD_Force';
+    CoVAll(:,i) =  CoV_Force';
+    
+    pxxAll(:,i) = mean(pxx);
 end
 
 figure(1)
@@ -54,7 +60,16 @@ ylabel('SD (N)','FontSize',14,'fontweight','bold')
 xlim([0 105])
 
 figure(4)
-errorbar(amp_vec*100, CoV_Force_all*100,CoV_Force_SD_all*100,'-','Color',[0.078,0,0.831],'LineWidth',2,'Marker','o','MarkerFaceColor',[0.078,0,0.831])
+errorbar(amp_vec*100,CoV_Force_all*100,CoV_Force_SD_all*100,'-','Color',[0.078,0,0.831],'LineWidth',2,'Marker','o','MarkerFaceColor',[0.078,0,0.831])
 xlabel('Excitation Level (%)','FontSize',14,'fontweight','bold')
 ylabel('CoV (%)','FontSize',14,'fontweight','bold')
 xlim([0 105])
+
+
+cd (dataFolder)
+save('mean_new_long','meanForceAll')
+save('CoV_new_long','CoVAll')
+save('std_new_long','stdAll')
+save('pxx_new_long','pxxAll')
+cd (codeFolder)
+
--- Analysis_Force_1.m
+++ Analysis_Force_1.m
@@ -6,7 +6,8 @@ dataFolder = '/Volumes/DATA2/Motor Unit Model Data/Twitch-based Muscle Model';
 codeFolder = '/Users/akira/Documents/GitHub/Twitch-Based-Muscle-Model';
 amp_vec = 0.1:0.1:1;
 
-Fs = 40000;
+%Fs = 1000;
+Fs = 50000;
 
 mean_Force_all = zeros(1,length(amp_vec));
 SD_Force_all = zeros(1,length(amp_vec));
@@ -14,7 +15,7 @@ SD_Force_SD_all = zeros(1,length(amp_vec));
 CoV_Force_all = zeros(1,length(amp_vec));
 CoV_Force_SD_all = zeros(1,length(amp_vec));
 
-for i = 1:length(amp_vec)
+for i = 2:length(amp_vec)
 %     cd (dataFolder)
 %     load(['Data_' num2str(i)],'Data')
 %     cd (codeFolder)
@@ -23,10 +24,11 @@ for i = 1:length(amp_vec)
    
     cd (dataFolder)
     for j = 1:10
-        load (['output_0_1_1_20_' num2str(i) '_' num2str(j) '.mat'])        
-        %Force(j,:) = sum(output.Force(:,5*Fs+2:end));   
-        Force(j,:) = output.Tendon_Force(5*Fs+2:end);   
-        Force_temp = output.Tendon_Force;
+        load (['output_FCR_long_0_1_1_10_' num2str(i) '_' num2str(j) '.mat'])        
+        Force(j,:) = sum(output.Force(:,5*Fs+2:end));   
+        %Force(j,:) = output.Tendon_Force(5*Fs+2:end);   
+        %Force_temp = output.Tendon_Force;
+        Force_temp  = sum(output.Force(:,5*Fs+2:end));
         clear output
     end
     cd (codeFolder)
@@ -36,7 +38,7 @@ for i = 1:length(amp_vec)
     hold on 
     
     cd (dataFolder)
-    save(['Force_0_1_1_20_' num2str(i)],'Force')
+    save(['Force_FCR_long_0_1_1_10_' num2str(i)],'Force')
     cd (codeFolder)
     i
     
