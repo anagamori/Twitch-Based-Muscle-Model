a079af13ec1e04c00223348c4e67f0e91821a3d6
Akira <nagamori@usc.edu>
2019-02-22 12:18:39 -0800

    new analysis code

---
+++ new_model_v11_parameterSearch_visualization.m
@@ -0,0 +1,34 @@
+%==========================================================================
+% new_model_v11_parameterSearch_visualization.m
+% Author: Akira Nagamori
+% Last update: 2/22/119
+% Descriptions
+%   Inspired by Williams et al. 1998
+%==========================================================================
+
+close all
+clear all
+clc
+
+code_folder = '/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model';
+data_folder = '/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model/Data';
+    
+%%
+for m = 1:1000
+    cd (data_folder)
+    load(['Data_' num2str(m)])   
+    cd(code_folder)
+    error_long(m) = Data{2,8};
+end
+
+%%
+index = find(error_long<0.4);
+for i = 1:length(index)
+    cd (data_folder)
+    load(['Data_' num2str(index(i))])   
+    cd(code_folder)
+    
+    CT_long(i) =  Data{2,1};
+end
+
+CT_long
