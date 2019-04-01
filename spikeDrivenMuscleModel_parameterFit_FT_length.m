9cc37de350c6e2b067abe760fae6f40fd9552c4a
Akira <nagamori@usc.edu>
2019-04-01 09:29:40 -0700

    new code for length for FT

--- spikeDrivenMuscleModel_parameterFit_FT_length.m
+++ spikeDrivenMuscleModel_parameterFit_FT_length.m
@@ -9,8 +9,8 @@ clear all
 clc
 
 %% Folder name
-code_folder = '/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model';
-data_folder = '/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model/Model Parameters/Model_1/ST';
+code_folder = '/Users/akira/Documents/GitHub/Twitch-Based-Muscle-Model';
+data_folder = '/Users/akira/Documents/GitHub/Twitch-Based-Muscle-Model/Model Parameters/Model_1/FT';
 %% Simulation parameters
 Fs = 1000; %sampling frequency
 T = 1/Fs;
@@ -18,12 +18,12 @@ time = 0:1/Fs:5; %simulation time
 
 %% Parameters to be searched
 %Lce = 1.1;
-first_MU = 38;
-last_MU = 38;
+first_MU = 62;
+last_MU = 195;
 
 param_target = [5 6 9 10];
 %numCores = feature('numcores');
-%parpool(3)
+%parpool(10)
 for trialN = first_MU:last_MU
     trialN
     cd(data_folder)
@@ -32,80 +32,61 @@ for trialN = first_MU:last_MU
     
     FR_half_initial = Data{2,6};
     param_initial = Data{2,12};
-    Lce_long = [0.8 0.9 1.1 1.2];
+    Lce_long = [0.8*ones(1,10) 0.9*ones(1,10) 1.1*ones(1,10) 1.2*ones(1,10)];
     Data_temp = cell(1,40);
     
-    M = randomizer(param_target,6,4);
-    
-    for j = 1
+    M = randomizer(param_target,6,40);
+    tic
+    parfor j = 1:40
         Lce = Lce_long(j);
         index_temp = M(j,:);
         
         FR_half = FR_half_initial;
-        if j == 1
-            param = param_initial;
-            param(5) = param(5)-0.2*param(5);
-            param(param_target(2:end)) = param(param_target(2:end))+0.2*param(param_target(2:end));
-        elseif j == 2
-            param = param_initial;
-            param(5) = param(5)-0.1*param(5);
-            param(param_target(2:end)) = param(param_target(2:end))+0.1*param(param_target(2:end));
-        elseif j == 3
-            param = param_initial;
-            param(5) = param(5)+0.1*param(5);
-            param(param_target(2:end)) = param(param_target(2:end))-0.1*param(param_target(2:end));
-        elseif j == 4
-            param = param_initial;
-            param(5) = param(5)+0.2*param(5);
-            param(param_target(2:end)) = param(param_target(2:end))-0.1*param(param_target(2:end));
-        end
-        min_error  = 200;
-        %while min_error > 20
-            for k = 1:6
-                rng shuffle
-                Param_matrix = annealing_curve(param,k);
-                
-                r = randperm(4);
-                
-                %% Loop through all parameters
-                for n = 1:length(r)
-                    index = index_temp(n+(k-1)*4);
-                    %% Loop through all perturbations
-                    error_long = zeros(1,3);
-                    param_temp = zeros(3,11);
-                    for i = 1:3
-                        [S,C,k_1,k_2,k_3,k_4,tau_1,tau_2,N,K,alpha] = parameter_Assigning(param,Param_matrix,index,i);
-                        param_temp(i,:) =  [S,C,k_1,k_2,k_3,k_4,tau_1,tau_2,N,K,alpha];
-                    end
-                    for l = 1:3
-                        
-                        %% Run a twitch simulation and sweep simulation
-                        [Data_temp_2] = spikeDrivenMuscleModel_testFunction(param_temp(l,:),Lce,FR_half,'slow',0);
-                        error_long(l) = Data_temp_2{2,8};
-                        
-                    end
-                    [min_error,loc_min_error] = min(error_long);
-                    [S,C,k_1,k_2,k_3,k_4,tau_1,tau_2,N,K,alpha] = parameter_Assigning(param,Param_matrix,index,loc_min_error);
-                    param =  [S,C,k_1,k_2,k_3,k_4,tau_1,tau_2,N,K,alpha];
-                end
-            end
-        %end
+        param = param_initial;
         
-        [Data_temp{j}] = spikeDrivenMuscleModel_testFunction(param,Lce,FR_half,'slow',1);
+        for k = 1:6
+
+            Param_matrix = annealing_curve(param,k);
+            %% Loop through all parameters
+            for n = 1:4
+                index = index_temp(n+(k-1)*4);
+                %% Loop through all perturbations
+                error_long = zeros(1,3);
+                param_temp = zeros(3,11);
+                for i = 1:3
+                    [S,C,k_1,k_2,k_3,k_4,tau_1,tau_2,N,K,alpha] = parameter_Assigning(param,Param_matrix,index,i);
+                    param_temp(i,:) =  [S,C,k_1,k_2,k_3,k_4,tau_1,tau_2,N,K,alpha];
+                end
+                for l = 1:3
+
+                    %% Run a twitch simulation and sweep simulation
+                    [Data_temp_2] = spikeDrivenMuscleModel_testFunction(param_temp(l,:),Lce,FR_half,'fast',0);
+                    
+                    error_long(l) = Data_temp_2{2,8};
+                    
+                end
+                [min_error,loc_min_error] = min(error_long);
+                [S,C,k_1,k_2,k_3,k_4,tau_1,tau_2,N,K,alpha] = parameter_Assigning(param,Param_matrix,index,loc_min_error);
+                param =  [S,C,k_1,k_2,k_3,k_4,tau_1,tau_2,N,K,alpha];
+            end
+        end
+       
+        [Data_temp{j}] = spikeDrivenMuscleModel_testFunction(param,Lce,FR_half,'fast',0);
         
     end
-    for trial = 1:4
+    for trial = 1:40
         Data  = Data_temp{trial};
         cd(data_folder)
         save(['Data_' num2str(trialN) '_' num2str(trial)],'Data')
         cd(code_folder)
     end
-    %close all
+    toc
+    close all
 end
 %%
 function Y = annealing_curve(x,k)
 
-perturbation_amp = 0.2/2;
+perturbation_amp = 0.3/2;
 
 Y(1,1) = x(1) +  (x(1)*perturbation_amp)./2.^(k-2);
 Y(2,1) = x(1) -  (x(1)*perturbation_amp)./2.^(k-2);
@@ -170,6 +151,7 @@ var11 = x(11);
 end
 
 function M = randomizer(x,n,m)
+rng shuffle
 M = zeros(m,n*length(x));
 for j = 1:m
     temp = [];
@@ -179,4 +161,3 @@ for j = 1:m
     M(j,:) = temp;
 end
 end
-
