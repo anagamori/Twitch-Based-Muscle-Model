64faa423c2a5d92c855b45253622b34e2eef71cc
Akira <nagamori@usc.edu>
2019-04-26 18:17:48 -0700

    data

--- run_muscleModel.m
+++ run_muscleModel.m
@@ -30,7 +30,7 @@ modelParameter.recruitment = 2; % 1: Loeb's formulation, 2: Fuglevand's formulat
 
 amp_vec = 0.1:0.1:1;
 trial_vec = [7 10];
-for j = 8:10 %7:length(amp_vec)
+for j = 6 %7:length(amp_vec)
     j
         if j <= 2
             Fs = 10000;
@@ -65,7 +65,7 @@ for j = 8:10 %7:length(amp_vec)
     input = [zeros(1,1*Fs) amp/2*[0:1/Fs:2] amp*ones(1,length(time)-1*Fs-length(amp*[0:1/Fs:2]))];
     %%
     
-    for i = 1:10
+    for i = 6:10
         tic
         output = spikeDrivenMuscleModel(Fs,time,input,modelParameter,1);
         toc
