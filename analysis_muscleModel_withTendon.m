e89e3b7f633cba54cded7d9e0e74cc1052ccfecf
Akira <nagamori@usc.edu>
2019-06-02 19:32:41 -0700

    data shortTendon

--- analysis_muscleModel_withTendon.m
+++ analysis_muscleModel_withTendon.m
@@ -10,7 +10,7 @@ clear all
 clc
 
 %%
-data_folder = '/Volumes/DATA2/New_Model/withTendon/10_CoV_50_Ur_Rec_3_noFV';
+data_folder = '/Volumes/DATA2/New_Model/withTendon/10_CoV_50_Ur_Rec_3_shortTendon';
 code_folder = '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model';
 figure_folder = '/Users/akira/Documents/GitHub/Twitch-Based-Muscle-Model/Figures';
 
@@ -24,36 +24,36 @@ cov_Force = zeros(10,length(amp_vec));
 pxx = zeros(10,1001);
 mean_pxx = zeros(length(amp_vec),1001);
 % %%
-for j = 7  %:length(amp_vec)
-    if j < 2
-        Fs = 10000;
-        time = 0:1/Fs:15;
-    elseif j >= 2 && j <= 3
-        Fs = 15000;
-        time = 0:1/Fs:15;
-    elseif j >= 4 && j < 7
-        Fs = 20000;
-        time = 0:1/Fs:15;
-    elseif j >= 7
-        Fs = 25000;
-        time = 0:1/Fs:15;
-    end
-%         if j <= 1
-%             Fs = 10000;
-%             time = 0:1/Fs:15;
-%         elseif j >= 2 && j <= 3
-%             Fs = 15000;
-%             time = 0:1/Fs:15;
-%         elseif j == 4
-%             Fs = 20000;
-%             time = 0:1/Fs:15;
-%         elseif j >= 5 && j < 7
-%             Fs = 25000;
-%             time = 0:1/Fs:15;
-%         elseif j >= 7
-%             Fs = 30000;
-%             time = 0:1/Fs:15;
-%         end
+for j = 5  %:length(amp_vec)
+%     if j < 2
+%         Fs = 10000;
+%         time = 0:1/Fs:15;
+%     elseif j >= 2 && j <= 3
+%         Fs = 15000;
+%         time = 0:1/Fs:15;
+%     elseif j >= 4 && j < 7
+%         Fs = 20000;
+%         time = 0:1/Fs:15;
+%     elseif j >= 7
+%         Fs = 25000;
+%         time = 0:1/Fs:15;
+%     end
+        if j <= 1
+            Fs = 10000;
+            time = 0:1/Fs:15;
+        elseif j >= 2 && j <= 3
+            Fs = 15000;
+            time = 0:1/Fs:15;
+        elseif j == 4
+            Fs = 20000;
+            time = 0:1/Fs:15;
+        elseif j >= 5 && j < 7
+            Fs = 25000;
+            time = 0:1/Fs:15;
+        elseif j >= 7
+            Fs = 30000;
+            time = 0:1/Fs:15;
+        end
 %     
     j
     tic
