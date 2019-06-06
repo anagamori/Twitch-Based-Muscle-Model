3e0aeb1b7384021686af9514ff4bb26406bff4ff
Akira <nagamori@usc.edu>
2019-06-06 14:53:35 -0700

    data

--- run_muscleModel.m
+++ run_muscleModel.m
@@ -11,9 +11,9 @@ clc
 
 
 %%
-data_folder = '/Volumes/DATA2/New_Model/withTendon/Model_4_10_CoV_50_Ur_Rec_3_shortTendon';
+data_folder = '/Volumes/DATA2/New_Model/withTendon/Model_4_10_CoV_50_Ur_Rec_3_constantT2T_shortTendon';
 code_folder = '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model';
-model_parameter_folder =  '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model/Model Parameters/Model_4_Ur_50_shortTendon';
+model_parameter_folder =  '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model/Model Parameters/Model_4_Ur_50_constantT2T_shortTendon';
 
 %%
 cd(model_parameter_folder)
@@ -30,7 +30,7 @@ modelParameter.recruitment = 3; % 1: Loeb's formulation, 2: Fuglevand's formulat
 
 amp_vec = [0.05 0.1:0.1:1];
 trial_vec = [7 10];
-for j = 10
+for j = 0:5
     j
 %     if j < 2
 %         Fs = 10000;
@@ -64,7 +64,19 @@ for j = 10
     amp = amp_vec(j+1);
     input = [zeros(1,1*Fs) amp/2*[0:1/Fs:2] amp*ones(1,length(time)-1*Fs-length(amp*[0:1/Fs:2]))];
     %%
-    if j <=2 
+%     if j <= 1
+%         for i = 2:10
+%             i
+%             tic
+%             output = spikeDrivenMuscleModel(Fs,time,input,modelParameter,1);
+%             toc
+%             cd(data_folder)
+%             save(['Data_' num2str(j) '_' num2str(i)],'output','-v7.3')
+%             cd(code_folder)
+%             clear output
+%             
+%         end
+%     else
         for i = 2:10
             i
             tic
@@ -76,17 +88,5 @@ for j = 10
             clear output
             
         end
-    else
-        for i = 1:10
-            i
-            tic
-            output = spikeDrivenMuscleModel(Fs,time,input,modelParameter,1);
-            toc
-            cd(data_folder)
-            save(['Data_' num2str(j) '_' num2str(i)],'output','-v7.3')
-            cd(code_folder)
-            clear output
-            
-        end
-   end
+%   end
 end
