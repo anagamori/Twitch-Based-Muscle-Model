e1a7b8d4bdd09caa8d79cb336af368eb02fa1b83
Akira <nagamori@usc.edu>
2019-06-13 09:52:57 -0700

    data n_100

--- analysis_muscleModel_withTendon.m
+++ analysis_muscleModel_withTendon.m
@@ -26,7 +26,7 @@ cov_Force = zeros(10,length(amp_vec));
 pxx = zeros(10,1001);
 mean_pxx = zeros(length(amp_vec),1001);
 % %%
-for j = 3 %:length(amp_vec)
+for j = 4:8 %:length(amp_vec)
     if j < 2
         Fs = 10000;
         time = 0:1/Fs:15;
