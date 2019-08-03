0a8124259b66253f969e767737dc78824bbbdcd2
Akira <nagamori@usc.edu>
2019-08-03 07:36:26 -0700

    data

--- run_muscleModel.m
+++ run_muscleModel.m
@@ -11,7 +11,7 @@ clc
 
 
 %%
-data_folder = '/Volumes/DATA2/New_Model/withTendon/Model_6_20_CoV_50_Ur_Rec_3_noFV';
+data_folder = '/Volumes/DATA2/New_Model/withTendon/Model_6_20_CoV_50_Ur_Rec_3';
 code_folder = '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model';
 model_parameter_folder =  '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model/Model Parameters/Model_6';
 
@@ -30,7 +30,7 @@ modelParameter.recruitment = 3; % 1: Loeb's formulation, 2: Fuglevand's formulat
 
 amp_vec = [0.05 0.1:0.1:1];
 trial_vec = [7 10];
-for j = 0:10
+for j = 4:10
     j
         if j < 2
             Fs = 10000;
@@ -70,8 +70,8 @@ for j = 0:10
     amp = amp_vec(j+1);
     input = [zeros(1,1*Fs) amp/2*[0:1/Fs:2] amp*ones(1,length(time)-1*Fs-length(amp*[0:1/Fs:2]))];
     %%
-    if j == 10
-        for i = 1:10
+    if j == 4
+        for i = 7:10
             i
             tic
             output = spikeDrivenMuscleModel(Fs,time,input,modelParameter,1);
