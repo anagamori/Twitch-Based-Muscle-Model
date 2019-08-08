a032162f1675f2a967233767f1a5e398afed0e51
Akira <nagamori@usc.edu>
2019-08-08 11:24:05 -0700

    data

--- analysis_muscleModel_withTendon.m
+++ analysis_muscleModel_withTendon.m
@@ -10,7 +10,7 @@ clear all
 clc
 
 %%
-condition = 'Model_6_10_CoV_50_Ur_Rec_3_PT';
+condition = 'Model_6_10_CoV_50_Ur_Rec_3';
 data_folder = ['/Volumes/DATA2/New_Model/withTendon/' condition];
 data_folder_git = ['/Users/akira/Documents/GitHub/Twitch-Based-Muscle-Model/Data/withTendon/' condition];
 code_folder = '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model';
@@ -27,7 +27,7 @@ pxx = zeros(10,1001);
 mean_pxx = zeros(length(amp_vec),1001);
 mean_pxx_2 = zeros(length(amp_vec),1001);
 % %%
-for j = 0:2 %:length(amp_vec)
+for j = 0:10 %:length(amp_vec)
     if j < 2
         Fs = 10000;
         time = 0:1/Fs:15;
--- run_muscleModel.m
+++ run_muscleModel.m
@@ -11,9 +11,9 @@ clc
 
 
 %%
-data_folder = '/Volumes/DATA2/New_Model/withTendon/Model_6_10_CoV_80_Ur_Rec_3';
+data_folder = '/Volumes/DATA2/New_Model/withTendon/Model_6_10_CoV_50_Ur_Rec_3_PT';
 code_folder = '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model';
-model_parameter_folder =  '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model/Model Parameters/Model_6_Ur_80';
+model_parameter_folder =  '/Users/akira/Documents/Github/Twitch-Based-Muscle-Model/Model Parameters/Model_6_PT';
 
 %%
 cd(model_parameter_folder)
@@ -30,7 +30,7 @@ modelParameter.recruitment = 3; % 1: Loeb's formulation, 2: Fuglevand's formulat
 
 amp_vec = [0.05 0.1:0.1:1];
 trial_vec = [7 10];
-for j = 10
+for j = 3:10
     j
         if j < 2
             Fs = 10000;
@@ -70,8 +70,8 @@ for j = 10
     amp = amp_vec(j+1);
     input = [zeros(1,1*Fs) amp/2*[0:1/Fs:2] amp*ones(1,length(time)-1*Fs-length(amp*[0:1/Fs:2]))];
     %%
-    if j == 10
-        for i = 4:10
+    if j == 3
+        for i = 5:10
             i
             tic
             output = spikeDrivenMuscleModel(Fs,time,input,modelParameter,1);
