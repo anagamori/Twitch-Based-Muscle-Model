bcf06e8a3db1a53b7d6ee101792f3ac1d8df69ac
Akira <nagamori@usc.edu>
2020-05-26 16:43:41 -0700

    update

--- Code for Figures/Todorov_2005.m
+++ Code for Figures/Todorov_2005.m
@@ -1,53 +1,66 @@
-close all
-clear all
-clc
+%==========================================================================
+% Todorov_2005.m
+% Author: Akira Nagamori
+% Last update: 3/1/20
+% Model descriptions:
+%   input: Fs = sampling frequency
+%             time = time vector
+%             synaptic_drive = synaptic drive to a motor unit population
+%             modelParameter = a structure that contains all model
+%             parameters
+%             fitOpt = figure display option (0: no figure output, 1: display
+%             figures)
+%   output: a data structure ('output') that contains spike trains, motor
+%   unit forces and tendon force (see l.242)
+%==========================================================================
 
-Fs = 100;
-time = 0:1/Fs:15;
+Fs = 100; % sampling frequency
+time = 0:1/Fs:15; % time vector
 
-amp_vec = 0.1:0.1:1;
+amp_vec = 0.1:0.1:1; % 
 
+% initialization
 mean_f = zeros(1,length(amp_vec));
 std_f = zeros(1,length(amp_vec));
 
 for i = 1:length(amp_vec)
-rng shuffle
-
-u = zeros(1,length(time));
-u(3*Fs+1:end) = amp_vec(i);
-r = normrnd(0,1,[1,length(time)]);
-omega_c = 0.5;
-
-tau_1 = 0.04;
-tau_2 = 0.04;
-
-mean_f_temp = zeros(1,20);
-std_f_temp = zeros(1,20);
-
-for j = 1:20
-f = 0;
-f_dot = 0;
-
-u_vec = zeros(1,length(time));
-f_vec = zeros(1,length(time));
-
-for t = 1:length(time)
-    u_t = u(t)*omega_c*r(t)+u(t);
-    f_ddot = (u_t - f - (tau_1+tau_2)*f_dot)/(tau_1*tau_2);
-    f_dot = f_ddot/Fs + f_dot;
-    f = f_dot/Fs + f;
+    rng shuffle
     
-    u_vec(t) = u_t;
-    f_vec(t) = f;
-end
-
-mean_f_temp(j) = mean(f_vec(3*Fs+1:end));
-std_f_temp(j) = std(f_vec(3*Fs+1:end));
-
-end
-mean_f(i) = mean(mean_f_temp);
-std_f(i) = mean(std_f_temp);
-
+    u = zeros(1,length(time));
+    u(3*Fs+1:end) = amp_vec(i);
+    r = normrnd(0,1,[1,length(time)]);
+    omega_c = 0.5;
+    
+    tau_1 = 0.04;
+    tau_2 = 0.04;
+    
+    mean_f_temp = zeros(1,20);
+    std_f_temp = zeros(1,20);
+    
+    for j = 1:20
+        f = 0;
+        f_dot = 0;
+        
+        u_vec = zeros(1,length(time));
+        f_vec = zeros(1,length(time));
+        
+        for t = 1:length(time)
+            u_t = u(t)*omega_c*r(t)+u(t);
+            f_ddot = (u_t - f - (tau_1+tau_2)*f_dot)/(tau_1*tau_2);
+            f_dot = f_ddot/Fs + f_dot;
+            f = f_dot/Fs + f;
+            
+            u_vec(t) = u_t;
+            f_vec(t) = f;
+        end
+        
+        mean_f_temp(j) = mean(f_vec(3*Fs+1:end));
+        std_f_temp(j) = std(f_vec(3*Fs+1:end));
+        
+    end
+    mean_f(i) = mean(mean_f_temp);
+    std_f(i) = mean(std_f_temp);
+    
 end
 
 figure(2)
--- Code for Figures/test_Af_length.m
+++ Code for Figures/test_Af_length.m
@@ -15,7 +15,7 @@ clc
 clear all
 %%
 code_folder = '/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model/Code for Figures';
-data_folder = '/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model/Model Parameters/Model_8';
+data_folder = '/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model/Model Parameters/Model_11';
 
 %% 
 cd(data_folder)
@@ -24,8 +24,8 @@ cd(code_folder)
 
 parameterMatrix = modelParameter.parameterMatrix;
 
-MU_No = 94;
-MU_type = 'fast';
+MU_No = 30;
+MU_type = 'slow';
 parameter = parameterMatrix(MU_No,:);
 %%
 
@@ -51,10 +51,34 @@ end
 
 freq = Data{2,9};
 fusion = 1-p2p/p2p(5,1);
+%% Data from McNulty et al. (2000)
+f_exp = [2 4 6 8 10 12.5 14 16 18 20 22 25 28 30 33.3 40 50 66.6 80 100];
+force_exp = [2.5723 7.717 16.399 29.582 44.373 50.482 62.379 63.344 67.203 73.234 77.492 83.923 87.46 88.103 91.318 96.141 97.428 100.32 100 98.392];
+[~,loc] = min(abs(force_exp-50));
+f_half_exp = f_exp(loc);
+fusion_exp = [-0.28169 17.465 36.62 58.31 74.93 85.915 90.141 92.394 94.93 96.056 96.62 97.465 97.465 97.746 98.028 97.746 97.465 97.465 97.746 96.62];
+
+%% Data from Macefield et al. (1993)
+% f_half_exp =  10.741;
+% f_exp = [2 5 8 10 15 20 30 50 80 100];
+% force_exp = [4.1775 14.36 31.854 44.909 64.752 76.762 86.945 91.906 92.689 94.256];
+% fusion_exp = [3.7123 27.842 48.956 67.981 83.527 88.167 88.631 95.128 95.592 95.592];
 %%
 figure()
 plot(freq*FR_half,fusion'*100,'LineWidth',2,'color','b')
 xlim([0 100])
+xlabel('Frequency (Hz)')
+ylabel('Fusion (%)')
+%xlim([0 3])
+set(gca,'TickDir','out');
+set(gca,'box','off')
+
+%%
+figure()
+plot(freq,(1-p2p(4,:)./p2p(4,1))*100,'LineWidth',1,'color','b')
+hold on 
+plot(f_exp./f_half_exp,fusion_exp,'LineWidth',1,'color','k')
+xlim([0 3])
 xlabel('Frequency (Hz)')
 ylabel('Fusion (%)')
 %xlim([0 3])
