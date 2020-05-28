3b4912adc5082c9dee4cf891fb9a3462f004dcba
Akira <nagamori@usc.edu>
2020-05-28 08:24:27 -0700

    update

---
+++ Code for Figures/DR_input.m
@@ -0,0 +1,98 @@
+%==========================================================================
+%PDRvsUth_nonlinear.m
+% Author: Akira Nagamori
+% Last update: 6/27/19
+% Descriptions:
+%   Plot the relationship between excitation and discharge rate of
+%   individual motor units
+%   Used to generate figure (onion_skinVsAHP)
+%==========================================================================
+close all
+clear all
+clc
+
+cd('/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model/Model Parameters/Model_11_v2');
+load('modelParameter')
+cd('/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model/Code for Figures')
+
+%%
+x = [11.025  21.277 50.097 72.34 93.617];
+DR_Connelly = [12.269 15.231 22.212 28.135 41.779];
+error_Connelly = [14.596 19.356 28.452 35.856 50.029];
+error_Connelly = error_Connelly - DR_Connelly;
+	
+%%
+MDR = modelParameter.MDR;
+PDR = modelParameter.PDR;
+U_th = modelParameter.U_th;
+index_slow = modelParameter.index_slow;
+
+%%
+g_e = modelParameter.g_e;
+lamda =  modelParameter.lamda;
+k_e =  modelParameter.k_e;
+index_saturation = modelParameter.index_saturation;
+U_th_t =  modelParameter.U_th_t;
+
+U_vec = x/100; %[0.1 0.25 0.5 0.75 1];
+DR_MU_all = zeros(200,length(U_vec));
+
+for i = 1:length(U_vec)
+
+DR_temp = zeros(modelParameter.N_MU,1);
+U = U_vec(i);
+DR_MU = g_e.*(U-U_th)+MDR;
+
+%    DR_temp = MDR + lamda.*k_e.*(U_vec(i)-U_th_new);
+for n = 1:length(index_saturation)
+    index = index_saturation(n);
+    if U <= U_th_t(index)
+        DR_temp(index) = MDR(index) + lamda(index).*k_e(index).*(U-U_th(index));
+    else
+        DR_temp(index) = PDR(index)-k_e(index)*(1-U);
+    end
+end
+
+DR_MU(index_saturation) = DR_temp(index_saturation);
+
+DR_MU(DR_MU<MDR) = nan;
+DR_MU(DR_MU>PDR) = PDR(DR_MU>PDR);
+
+DR_MU_all(:,i) = DR_MU;
+
+end
+
+
+
+%%
+figure(1)
+% boxplot(DR_MU_all,'positions',[10 25 50 75 100])
+% hold on 
+% scatter(10*ones(size(DR_MU_all(:,1))).*(1+(rand(size(DR_MU_all(:,1)))-0.5)/10),DR_MU_all(:,1),'r','filled')
+
+errorbar(x+1,nanmean(DR_MU_all),nanstd(DR_MU_all),'LineWidth',1,'Color',[37  65 178]/255)
+hold on
+errorbar(x,DR_Connelly,error_Connelly,'LineWidth',1,'Color','k')
+xlabel('Synaptic Input (%Maximum)','FontSize',12)
+ylabel('Dsicharge Rate (Hz)','FontSize',12)
+legend('New Model','Connelly et al. 1999')
+set(gca,'TickDir','out');
+set(gca,'box','off')
+% scatter(U_th_vec,DR_vec,'filled','k','LineWidth',0.5)
+% hold on
+% plot(U_th_vec,DR_Calc,'r','LineWidth',1)
+% xlim([-1 11])
+% xticks([0 0.05 0.1 0.15 0.2]*100)
+% xlabel('Recruitment Threshold (%Maximum)','FontSize',8)
+% ylabel('Discharge Rate (Hz)','FontSize',8)
+% set(gca,'TickDir','out');
+% set(gca,'box','off')
+% ax = gca;
+% ax.FontSize = 6;
+% fig = gcf;
+% fig.PaperUnits = 'inches';
+% fig.PaperPosition = [0 0 3.34 3.34];
+
+
+
+
--- Code for Figures/modelProperties_Scatter_v2.m
+++ Code for Figures/modelProperties_Scatter_v2.m
@@ -10,7 +10,7 @@ close all
 clear all
 clc
 
-cd('/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model/Model Parameters/Model_11');
+cd('/Users/akiranagamori/Documents/GitHub/Twitch-Based-Muscle-Model/Model Parameters/Model_11_v2');
 load('modelParameter')
 load('CT_vec')
 load('FR_half')
@@ -24,12 +24,15 @@ N_MU = modelParameter.N_MU;
 CT = CT_vec(index_MU)';
 t2t = t2t(index_MU)';
 FR_half = modelParameter.FR_half;
+MDR = FR_half/2;
+PDR = FR_half*2;
 PTi = modelParameter.PTi;
 index_slow = modelParameter.index_slow;
 U_th = modelParameter.U_th;
 
 %%
 %randperm(300,50)
+
 figure(1)
 plot(CT(1:index_slow),PTi(1:index_slow),'o','color',[36 123 160]/255,'LineWidth',1)
 hold on 
@@ -89,18 +92,20 @@ ax = gca;
 ax.FontSize = 10;
 
 %%
-mdl_CT_FR_half = fitlm(CT,FR_half);
+ISI = 1./FR_half*1000;
+mdl_CT_FR_half = fitlm(CT,ISI);
 betahat_CT_FR_half = mdl_CT_FR_half.Coefficients.Estimate;
 
 figure(5)
-plot(CT(1:index_slow),FR_half(1:index_slow),'o','color',[36 123 160]/255,'LineWidth',1)
+plot(CT(1:index_slow),ISI(1:index_slow),'o','color',[36 123 160]/255,'LineWidth',1)
 hold on 
-plot(CT(index_slow+1:end),FR_half(index_slow+1:end),'o','color',[255 22 84]/255,'LineWidth',1)
+plot(CT(index_slow+1:end),ISI(index_slow+1:end),'o','color',[255 22 84]/255,'LineWidth',1)
 plot(CT,betahat_CT_FR_half(1)+CT*betahat_CT_FR_half(2),'k','LineWidth',1)
-text(mean(CT),45,num2str(sqrt(mdl_CT_FR_half.Rsquared.Ordinary)))
-ylim([0 45])
+text(80,90,num2str(sqrt(mdl_CT_FR_half.Rsquared.Ordinary)))
+text(80,80,num2str(betahat_CT_FR_half(2)))
+%ylim([0 45])
 xlabel('Contraction Time (ms)','FontSize',14)
-ylabel('f_{0.5} (Hz)','FontSize',14)
+ylabel('Stimulus Interval at f_{0.5} (ms)','FontSize',14)
 set(gca,'TickDir','out');
 set(gca,'box','off')
 ax = gca;
@@ -108,9 +113,9 @@ ax.FontSize = 10;
 
 %%
 figure(6)
-histogram(PTi(1:index_slow),0:0.1:2,'FaceColor',[36 123 160]/255)
+histogram(PTi(1:index_slow),'FaceColor',[36 123 160]/255)
 hold on
-histogram(PTi(index_slow+1:end),0:0.1:2,'FaceColor',[255 22 84]/255)
+histogram(PTi(index_slow+1:end),'FaceColor',[255 22 84]/255)
 xlabel('Peak Tetanic Force','FontSize',14)
 ylabel('Counts','FontSize',14)
 legend('Slow-twitch','Fast-twitch')
@@ -120,14 +125,92 @@ ax = gca;
 ax.FontSize = 10;
 
 %%
+x = 22.5:5:87.5;
+frac_Cuseum = [0.016941 0.056471 0.13882 0.19859 0.17129 0.16094 0.11059 0.049882 0.024471 0.020706 0.024471 0.016941 0.0056471 0.0089412];
+
+int = 20:5:90;
+count_slow = zeros(1,length(x));
+count_fast = zeros(1,length(x));
+for i = 1:length(int)-1
+    count_slow(i) = length(find(CT(1:index_slow)>=int(i)&CT(1:index_slow)<int(i+1)));
+    count_fast(i) = length(find(CT(index_slow+1:end)>=int(i)&CT(index_slow+1:end)<int(i+1)));
+end
+frac_slow = count_slow./(sum(count_slow)+sum(count_fast));
+frac_fast = count_fast./(sum(count_slow)+sum(count_fast));
 figure(7)
-histogram(CT(1:index_slow),10:10:120,'FaceColor',[36 123 160]/255)
-hold on
-histogram(CT(index_slow+1:end),10:10:120,'FaceColor',[255 22 84]/255)
-xlabel('Contraction Time (s)','FontSize',14)
-ylabel('Counts','FontSize',14)
-legend('Slow-twitch','Fast-twitch')
+h = bar(x,frac_slow,'hist');
+set(h,'FaceColor',[36 123 160]/255,'FaceAlpha',0.5);
+hold on 
+h2 = bar(x,frac_fast,'hist');
+set(h2,'FaceColor',[255 22 84]/255,'FaceAlpha',0.5);
+plot(x,frac_Cuseum,'k','LineWidth',1)
+plot(x,frac_Cuseum,'o', 'Color','k','MarkerFaceColor', 'k')
+xlabel('Contraction Time (s)','FontSize',10)
+ylabel('Proportion','FontSize',14)
+legend('Slow-twitch','Fast-twitch','Cutsem et al. 1993')
 set(gca,'TickDir','out');
 set(gca,'box','off')
 ax = gca;
 ax.FontSize = 10;
+
+%%
+x = 5:2:15;
+frac_Cutsem = [0.17846 0.20369 0.23015 0.17908 0.11508 0.038769];
+int = 2:2:20;
+count_slow = zeros(1,length(int)-1);
+count_fast = zeros(1,length(int)-1);
+for i = 1:length(int)-1
+    count_slow(i) = length(find(MDR(1:index_slow)>=int(i)&MDR(1:index_slow)<int(i+1)));
+    count_fast(i) = length(find(MDR(index_slow+1:end)>=int(i)&MDR(index_slow+1:end)<int(i+1)));
+end
+frac_slow = count_slow./(sum(count_slow)+sum(count_fast));
+frac_fast = count_fast./(sum(count_slow)+sum(count_fast));
+
+figure(8)
+h = bar(3:2:19,frac_slow,'hist');
+set(h,'FaceColor',[36 123 160]/255,'FaceAlpha',0.5);
+set(gca,'XTick',2:2:20)
+hold on 
+h2 = bar(3:2:19,frac_fast,'hist');
+set(h2,'FaceColor',[255 22 84]/255,'FaceAlpha',0.5);
+plot(x,frac_Cutsem,'k','LineWidth',1)
+plot(x,frac_Cutsem,'o', 'Color','k','MarkerFaceColor', 'k')
+xlabel('MDR (Hz)','FontSize',10)
+ylabel('Proportion','FontSize',14)
+legend('Slow-twitch','Fast-twitch','Cutsem et al. 1993')
+set(gca,'TickDir','out');
+set(gca,'box','off')
+ax = gca;
+ax.FontSize = 10;
+
+%%
+x = 15:10:65;
+frac_Cutsem = [0.071464 0.26427 0.30149 0.14516 0.12134 0.026055];
+
+int = 10:10:80;
+count_slow = zeros(1,length(int)-1);
+count_fast = zeros(1,length(int)-1);
+for i = 1:length(int)-1
+    count_slow(i) = length(find(PDR(1:index_slow)>=int(i)&PDR(1:index_slow)<int(i+1)));
+    count_fast(i) = length(find(PDR(index_slow+1:end)>=int(i)&PDR(index_slow+1:end)<int(i+1)));
+end
+frac_slow = count_slow./(sum(count_slow)+sum(count_fast));
+frac_fast = count_fast./(sum(count_slow)+sum(count_fast));
+
+figure(9)
+h = bar(15:10:75,frac_slow,'hist');
+set(h,'FaceColor',[36 123 160]/255,'FaceAlpha',0.5);
+set(gca,'XTick',10:10:80)
+hold on 
+h2 = bar(15:10:75,frac_fast,'hist');
+set(h2,'FaceColor',[255 22 84]/255,'FaceAlpha',0.5);
+plot(x,frac_Cutsem,'k','LineWidth',1)
+plot(x,frac_Cutsem,'o', 'Color','k','MarkerFaceColor', 'k')
+xlabel('PDR (Hz)','FontSize',10)
+ylabel('Proportion','FontSize',14)
+legend('Slow-twitch','Fast-twitch','Cutsem et al. 1993')
+set(gca,'TickDir','out');
+set(gca,'box','off')
+ax = gca;
+ax.FontSize = 10;
+
